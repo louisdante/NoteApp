32d42198de6f20f60f05744a498277e3766ab59d
louisdante <mase2kool@yahoo.com.au>
2016-08-17 12:01:38 +0100

    create a method to search and delete items on the note app

--- noteApp.js
+++ noteApp.js
@@ -22,5 +22,24 @@ class NotesApplication {
             }
         }
     }
+    search(search_text) {
+        console.log("Showing results for search " + search_text);
 
+        for (var i = 0; i < this.notes.length; i++) {
+            if (this.notes[i].search(search_text)) {
+
+                console.log("note ID: " + i + "\n" + this.notes[i] + "\n\n" + "By Author " + this.author);
+            }
+
+        }
+
+    }
+    delete(note_id) {
+        if (note_id > this.notes.length || note_id < 0) {
+            return "the number entered doesnt exit";
+        } else {
+            this.notes.splice(note_id);
+            return "deleted";
+        }
+    }
 }
