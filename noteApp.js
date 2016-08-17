a8dfdf697e5db8a14123642221a93b3f65ad0821
louisdante <mase2kool@yahoo.com.au>
2016-08-17 11:22:34 +0100

    Created a method lists out each of the notes in the notes list

--- noteApp.js
+++ noteApp.js
@@ -7,6 +7,12 @@ class NotesApplication {
         this.notes.push(note_content);
         return "created";
     }
-    
+    listNotes() {
+        for (var i = 0; i < this.notes.length; i++) {
+
+            console.log("note ID: " + i + "\n" + this.notes[i] + "\n\n" + "By Author " + this.author);
+        }
+    }
+
 
 }
