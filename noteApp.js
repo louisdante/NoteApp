cc4f6ebb9a5eefcec06488084d99f30bfffbce5e
louisdante <mase2kool@yahoo.com.au>
2016-08-17 11:14:38 +0100

    Create a method that create a note content

---
+++ noteApp.js
@@ -0,0 +1,13 @@
+class NotesApplication {
+    constructor(author, notes) {
+        this.author = author;
+        this.arr = arr;
+    }
+    create(note_content) {
+        this.notes.push(note_content);
+        return "created";
+    }
+
+
+
+}
