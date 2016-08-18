078ad6259ec2d39a79fd71983c1d1de1043e3e16
louisdante <mase2kool@yahoo.com.au>
2016-08-18 23:37:45 +0100

    added describe function on my spec file to test methods in the noteapp file

--- noteApp.js
+++ noteApp.js
@@ -1,7 +1,20 @@
+/**
+ * @Author: Louis nwamadi;
+ * @App-name: NotesApplication;
+ * @Description: A NotesApplication app that creates a note,
+ * lists, edit, gets a particular content, searches for a particular  
+ * word in the note database and delete a note....
+ * 
+ * @version:1.1.0;
+ * @license: MIT;
+ * 
+ */
+
 var NotesApplication = function(author) {
 
     this.author = author;
     this.notes = [];
+
 };
 
 //
@@ -11,8 +24,9 @@ NotesApplication.prototype.create = function(note_content) {
 };
 NotesApplication.prototype.listNotes = function() {
     for (var i = 0; i < this.notes.length; i++) {
-
-        console.log("note ID: " + i + "\n" + this.notes[i] + "\n\n" + "By Author " + this.author);
+        var result = '';
+        result += "note ID: " + i + "\n" + this.notes[i] + "\n\n" + "By Author " + this.author;
+        return result;
     }
 };
 
@@ -21,7 +35,9 @@ NotesApplication.prototype.get = function(note_id) {
     var arr = this.notes;
     for (var i = 0; i < arr.length; i++) {
         if (i === note_id) {
-            return '*-----* ' + arr[i] + ' *-------*';
+            var value
+            value += '*-----* ' + arr[i] + ' *-------*';
+            return valuel;
         }
     }
 };
@@ -31,16 +47,17 @@ NotesApplication.prototype.search = function(search_text) {
     console.log("Showing results for search " + search_text);
 
     for (var i = 0; i < this.notes.length; i++) {
-        if (this.notes[i].search(search_text)) {
-
-            console.log("note ID: " + i + "\n" + this.notes[i] + "\n\n" + "By Author " + this.author);
+        if (this.notes[i].match(search_text)) {
+            var result = '';
+            result += "note ID: " + i + "\n" + this.notes[i] + "\n\n" + "By Author " + this.author;
+            return result;
         }
 
     }
 
 };
 
-//
+//work on this function to bring desired result
 NotesApplication.prototype.delete = function(note_id) {
     if (note_id > this.notes.length || note_id < 0) {
         return "the number entered doesnt exit";
--- test/notes-spec.js
+++ test/notes-spec.js
@@ -2,8 +2,8 @@
 var Note = require('../noteApp.js');
 var expect = require('chai').expect;
 var app = new Note('louis');
+console.log(app);
 describe("The NotesApp ", function() {
-
     it("expects value to be a string", function() {
         expect(app.author).to.be.a("string");
     });
@@ -12,15 +12,85 @@ describe("The NotesApp ", function() {
 describe("The create method", function() {
     it("should only accept strings", function() {
         expect(app.create("This is a text.")).to.be.a("string");
+        console.log(app.notes);
     });
+
 });
+
 describe("The listNotes method", function() {
     it('should be a property of the noteApp class and ...', function() {
         expect(Note.prototype.hasOwnProperty('listNotes')).to.be.true;
+
     });
-    app.create("text 1");
-    app.create("text 2");
-    it("should return true if not empty", function() {
-        expect(app.listNotes()).to.be.a("string");
+    it(" has to get a note with an index that exists", function() {
+        var result = app.listNotes();
+        expect(result).not.to.be.null;
+    });
+
+});
+describe("The get method", function() {
+    it('should be a property of the noteApp class and', function() {
+        expect(Note.prototype.hasOwnProperty('get')).to.be.true;
+
+    });
+    it(" has to get a note with an index that exists", function() {
+        expect(app.get()).not.to.be.null;
+        expect(Note).to.respondTo('get');
+    });
+    it(" has to get a note with an index that exists", function() {
+        var result = app.get(1);
+        expect(app.notes[1]).to.equal(result);
+    });
+});
+
+describe("The search method", function() {
+    it('be a property of the noteApp class and..', function() {
+        expect(Note.prototype.hasOwnProperty('search')).to.be.true;
+    });
+
+    it("should be a match string", function() {
+        app.create("this is a new test");
+        var result = app.search('is');
+        expect(result).to.include('is');
+        // expect(app.search()).to.be.undefined;
+    });
+    it('should log an error if parameter passed is not a string', function() {
+        var result = app.search(1);
+        expect(result).to.not.be.ok;
+    });
+
+});
+
+describe("delete", function() {
+    it('should be a property of the noteApp class and..', function() {
+        expect(Note.prototype.hasOwnProperty('delete')).to.be.true;
+    });
+    it("the deleted note at index should not exists", function() {
+        app.create("First time");
+        app.create("Second time");
+        console.log(app.notes);
+        //expect(app.delete(0)).to.equal('deleted');
+        console.log(app.notes);
+    });
+    it(" has to delete a note with an index that exists", function() {
+        expect(app.delete()).not.to.be.null;
+        expect(Note).to.respondTo('delete');
+    });
+
+
+});
+describe("The edit method", function() {
+    it('should be a property of the noteApp class and..', function() {
+        expect(Note.prototype.hasOwnProperty('delete')).to.be.true;
+    });
+
+    it('should replace index value with new text', function() {
+        app.create("First time");
+        app.notes[0] = 'louis got served';
+        expect(app.notes[0]).to.equal('louis got served')
+    });
+    it(" has to delete a note with an index that exists", function() {
+        expect(app.edit()).not.to.be.null;
+        expect(Note).to.respondTo('edit');
     });
 });
